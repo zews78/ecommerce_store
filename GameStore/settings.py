d1a8af1e27eb4e20f21ef0c6e6acc22b84d455d8
roneetsingh <kaket786@gmail.com>
2021-06-26 19:29:23 +0530

    Second

--- GameStore/settings.py
+++ GameStore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)$9)!pjgl2br#e$&ea6o2t0gpkg9$fl=i0x=mf_4#f#l#wd)o#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
