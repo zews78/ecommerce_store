a027895a10ac63b741e097903d3844a765fd5a88
roneetsingh <kaket786@gmail.com>
2021-06-27 00:04:22 +0530

    changes

--- GameStore/settings.py
+++ GameStore/settings.py
@@ -21,12 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)$9)!pjgl2br#e$&ea6o2t0gpkg9$fl=i0x=mf_4#f#l#wd)o#'
-
-#os.environ['SECRET_KEY']
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["gamestoredj.herokuapp.com", "localhost"]
 
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: waitress-serve --port=8000 GameStore.wsgi:application
+web: waitress-serve --port=$POST GameStore.wsgi:application
